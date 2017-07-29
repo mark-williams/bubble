d56230d346851496ddd367dd8f18cc49cead356f
mark-williams <markwill@hotmail.co.uk>
2017-07-29 11:06:21 +0100

    SortContainer now a stateful component as this will perform the sort and maintain the current state of thelist.

--- src/components/SortContainer/SortContainer.js
+++ src/components/SortContainer/SortContainer.js
@@ -2,12 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SortList from '../SortList';
 
-const SortContainer = ({ numbersToSort }) => (
-  <div>
-    <h3>Sorting...</h3>
-    <SortList list={numbersToSort} />
-  </div>
-);
+class SortContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: this.props.numbersToSort };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Sorting...</h3>
+        <SortList list={this.state.items} />
+      </div>
+    );
+  }
+}
 
 SortContainer.propTypes = {
   numbersToSort: PropTypes.arrayOf(PropTypes.number),
--- src/components/SortList/SortList.js
+++ src/components/SortList/SortList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SortList = ({ list }) => (
   <div className="sortList">
     {list.map(n => <div className="sortItem">{n}</div>)}
   </div>
 );
+
+SortList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default SortList;
