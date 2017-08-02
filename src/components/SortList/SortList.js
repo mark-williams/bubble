88f8266916b77c0f37fe777eaa0f84a3cb51c04c
mark-williams <markwill@hotmail.co.uk>
2017-08-02 09:35:38 +0100

    SortList uses styled components

--- src/components/SortList/SortList.js
+++ src/components/SortList/SortList.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components'; 
 import SortItem from '../SortItem';
 
+const StyledList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const SortList = ({ list }) => (
-  <div className="sortList">
+  <StyledList>
     {list.map(x => (<SortItem key={x.id} item={x.number} />))}
-  </div>
+  </StyledList>
 );
 
 SortList.propTypes = {
