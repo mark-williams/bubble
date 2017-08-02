0ba8eddddc189098465b5f3db423999d6443f706
mark-williams <markwill@hotmail.co.uk>
2017-08-02 09:42:50 +0100

    Linting

--- src/components/SortList/SortList.js
+++ src/components/SortList/SortList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components'; 
+import styled from 'styled-components';
 import SortItem from '../SortItem';
 
 const StyledList = styled.div`
