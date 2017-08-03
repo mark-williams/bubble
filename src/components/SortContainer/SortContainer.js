c7250a53116dbd1f9f860b4b1854563c58e7f154
mark-williams <markwill@hotmail.co.uk>
2017-08-03 12:04:20 +0100

    Investigate use of recompose

--- src/components/SortContainer/SortContainer.js
+++ src/components/SortContainer/SortContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { compose, lifecycle, withReducer } from 'recompose';
 import styled from 'styled-components';
 import SortList from '../SortList';
 import bubble from '../../services/sort';
@@ -12,40 +13,52 @@ const Status = styled.h3`
   font-style: ${props => (props.isSorted ? 'italic' : 'normal')};
 `;
 
-class SortContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: getItems(this.props.numbersToSort), sorted: false };
-  }
-
-  componentDidMount() {
-    this.sorted = false;
-    this.interval = setInterval(this.sort, 180);
-  }
-
-  sort = () => {
-    const items = this.state.items.slice();
-    const results = bubble(items, comparer);
-    if (results.sorted) {
-      clearInterval(this.interval);
-    }
-
-    this.setState(results);
-  }
-
-  render() {
-    const message = this.state.sorted ? <span>Sorted!</span> : <span>Sorting...</span>;
-    return (
-      <div>
-        <Status isSorted={this.state.sorted} className="sort-status">{message}</Status>
-        <SortList list={this.state.items} />
-      </div>
-    );
-  }
-}
-
-SortContainer.propTypes = {
-  numbersToSort: PropTypes.arrayOf(PropTypes.number),
+const INITIAL = {
+  items: [],
+  sorted: false,
 };
 
-export default SortContainer;
+const INIT = 'INIT';
+const SORT = 'SORT';
+
+const sortedReducer = (state = INITIAL, action) => {
+  switch (action.type) {
+    case INIT:
+      return Object.assign({}, state, { items: getItems(action.items) });
+
+    case SORT:
+      return bubble(state.items.slice(), comparer);
+
+    default:
+      return state;
+  }
+};
+
+const FlagSorted = withReducer('sortState', 'dispatch', sortedReducer);
+const Timed = lifecycle({
+  componentWillMount() {
+    this.props.dispatch({ type: INIT, items: this.props.numbersToSort });
+  },
+  componentDidMount() {
+    setInterval(() => {
+      this.props.dispatch({ type: SORT });
+    }, 
+    100);
+  },
+});
+
+const SortContainer = ({ sortState }) => {
+  const message = sortState.sorted ? 'Sorted' : 'Sorting...';
+  return (
+    <div>
+      <Status className="sort-status">{message}</Status>
+      <SortList list={sortState.items} />
+    </div>
+  );
+};
+
+SortContainer.propTypes = {
+  sortState: PropTypes.object,
+};
+
+export default compose(FlagSorted, Timed)(SortContainer);
